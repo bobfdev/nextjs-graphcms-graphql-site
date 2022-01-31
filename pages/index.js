923e25b567a0c64642f6d355b3b9049ec9af8ddb
Bob F <bobfdev@gmail.com>
2022-01-30 22:24:24 -0600

    updated files

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import Head from 'next/head'
+import { gql } from '@apollo/client';
+import Head from 'next/head';
+import client from '../apolloClient';
 
-
-export default function Home() {
+export default function Home({ nextjsProjects }) {
+  console.log(nextjsProjects);
   return (
     <div>
       <Head>
@@ -10,7 +12,40 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Learning Nextjs</h1>
-
+      <ul>
+        {nextjsProjects.map((nextjsProject, i) => 
+          <li key={i}>
+            <a href={nextjsProject.slug}>{nextjsProject.title}</a>
+          </li>
+        )}
+      </ul>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const {data} = await client.query({
+    query: gql`
+      query {
+        nextjsProjects {
+          title
+          slug
+          dateAndTime
+          coverImage {
+            url
+          }
+          guestName
+          description {
+            raw
+          }
+        }
+      }
+    `
+  })
+  const {nextjsProjects} = data;
+    return {
+      props: {
+        nextjsProjects
+      }
+    }
 }
